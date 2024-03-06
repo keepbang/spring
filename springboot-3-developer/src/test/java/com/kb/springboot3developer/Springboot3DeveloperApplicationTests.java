c7d81403d321ffe2b5e226808cbf9acac1da7ee1
keepbang <rlqud1125@gmail.com>
2024-03-06 09:36:56 +0900

    feat: add springboot 3 develop

---
+++ springboot-3-developer/src/test/java/com/kb/springboot3developer/Springboot3DeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.springboot3developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3DeveloperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
